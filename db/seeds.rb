1eb988f8f206ce48cec343163033a73143e08c7f
Erick <erisrnb18@gmail.com>
2021-09-15 20:14:50 -0400

    removing additional git pull

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,6 @@
 Movie.create!(title: 'Inception', description: 'In a world where technology exists to enter the human mind through dream invasion, a highly skilled thief is given a final chance at redemption which involves executing his toughest job to date: Inception.', movie_length: '2h 28m', director: 'Christopher Nolan', rating: '', user_id: '' )
 Movie.create!(title: 'The Avengers', description: 'Nick Fury of S.H.I.E.L.D. brings together a team of super humans to form The Avengers to help save the Earth from Loki and his army.', movie_length: '2h 23m', director: 'Joss Whedon', rating: '', user_id: '' )
 Movie.create!(title: 'The Matrix', description: 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.', movie_length: '2h 16m', director: 'Lana Wachowski and Lilly Wachowski', rating: '', user_id: '' )
-<<<<<<< HEAD
 Movie.create!(title: 'Shaun of the Dead', description: 'A man decides to turn his moribund life around by winning back his ex-girlfriend, reconciling his relationship with his mother, and dealing with an entire community that has returned from the dead to eat the living.', movie_length: '1h 39m', director: 'Edgar Wright', rating: '', user_id: '' )
 
- 
-=======
-Movie.create!(title: 'Shaun of the Dead', description: 'A man decides to turn his moribund life around by winning back his ex-girlfriend, reconciling his relationship with his mother, and dealing with an entire community that has returned from the dead to eat the living.', movie_length: '1h 39m', director: 'Edgar Wright', rating: '', user_id: '' )
->>>>>>> a791c5dcc86ecc0dfd2880fb6ebc264440ff1c25
+
